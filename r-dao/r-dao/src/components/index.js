b66614090460f3eb43056476011d1501fbbfe883
maxpru1991 <maxi.prudenzano91@gmail.com>
2023-04-12 18:41:24 +0100

    setup gh-pages

--- r-dao/r-dao/src/components/index.js
+++ r-dao/r-dao/src/components/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from "react-router-dom";
 
 
 
-
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
