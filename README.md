6d61aa77f731ffe53238eae5eddf73e84ff34346
maxpru1991 <90486434+maxpru1991@users.noreply.github.com>
2023-04-12 23:09:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# r-dao
+# r-dao
+# r-dao
+# r-dao
+# R-DAO-REACT
--- r-dao/r-dao/src/components/index.js
+++ r-dao/r-dao/src/components/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = createRoot(document.getElementById('root'));
 root.render(
     <Router>
       <App />
